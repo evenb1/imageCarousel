de1a455b9fca9457a07b2ddc2a34e237bb300695
Even <even.berhane2022@gmail.com>
2024-04-29 21:34:07 +0300

    dot variable

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ function showImage(i){
     var images = document.getElementsByName(image);
     for(i=0;i<images.length;i++)
     images[i].style.display='none';
+
+    var dots = document.getElementsByName("dot");
 }
